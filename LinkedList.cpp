8f475594eed9151fa858f557993bfc2a30ac87df
Ronaldo <rraragaomonteiro@gmail.com>
2024-07-30 01:07:14 -0300

    initial commit

---
+++ LinkedList.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <ostream>
+
+class Node {
+public:
+  int data;
+
+  Node *next;
+
+  Node(int data) {
+    this->data = data;
+    this->next = nullptr;
+  }
+};
+
+class LinkedList {
+private:
+  Node *head;
+
+public:
+  LinkedList() { this->head = nullptr; }
+
+  void insertAtBeginning(int value) {
+    Node *newNode = new Node(value);
+    newNode->next = head;
+    head = newNode;
+  };
+
+  void insertAtEnd(int value) {
+    Node *newNode = new Node(value);
+    if (head == nullptr) {
+      head = newNode;
+      return;
+    }
+
+    Node *temp = head;
+    while (temp->next != nullptr) {
+      temp = temp->next;
+    }
+    temp->next = newNode;
+  };
+
+  void insert(int value, int pos) {
+    if (pos > this->getListLength() - 1 || pos < 0) {
+      std::cout << "Invalid Pos" << std::endl;
+    } else {
+      Node *temp = new Node(value);
+      Node *temp2 = head;
+      for (int i = 0; i < pos - 1; i++) {
+        temp2 = temp2->next;
+      }
+      temp->next = temp2->next;
+      temp2->next = temp;
+    }
+  }
+
+  void traverseLinkedList() {
+    Node *temp = head;
+    while (temp != nullptr) {
+      std::cout << temp->data << " ";
+      temp = temp->next;
+    }
+    std::cout << std::endl;
+  };
+
+  int getListLength() {
+    int length = 0;
+    Node *current = head;
+    while (current != nullptr) {
+      length++;
+      current = current->next;
+    };
+
+    return length;
+  }
+};
+
+int main() {
+  LinkedList *list = new LinkedList();
+  list->insertAtBeginning(10);
+  list->insertAtBeginning(12);
+  list->insertAtBeginning(14);
+  list->insertAtBeginning(16);
+  list->insert(24, 3);
+  list->insert(5, 3);
+  list->traverseLinkedList();
+  return 0;
+};
